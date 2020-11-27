f0ced652731f327abf308e9c42dcf5fc5646e3c3
Ricardo Rosa <rjrosa@outlook.com>
2020-11-27 08:21:57 +0000

    committing the Pong file the screen has been created.

--- Pong.py
+++ Pong.py
@@ -1,2 +1,22 @@
-import pygame
+import pygame, sys
+
+pygame.init()
+
+WIDTH = 900
+HEIGHT = 600
+BLACK = (0, 0, 0)
+WHITE = (255, 255, 255)
+
+screen = pygame.display.set_mode((WIDTH, HEIGHT))
+
+screen.fill(BLACK)
+pygame.display.set_caption('Pong!')
+
+#main loop
+
+while True:
+    pygame.display.update()
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            sys.exit()
 
